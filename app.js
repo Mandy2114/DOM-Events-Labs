1a55e54b3b9c78681ee6dc9860982f69223bd89b
Mandy DDEcamp <Mandydecamp1213@gmail.com>
2024-09-11 14:13:07 -0400

    Stuck

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const calculatorButtonElement = document.querySelector("#calculator");
+const row = document.querySelector(".row");
+const Apples = document.querySelector("");
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
